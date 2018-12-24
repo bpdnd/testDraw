90047530c52d4885ec02bfe24dca8189e29ef53e
bpdnd <3438291751@qq.comgit config --global user.email 3438291751@qq.com>
2018-12-24 10:03:00 +0800

    初始化

---
+++ testDraw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testDraw
+//
+//  Created by Admin on 2018/12/24.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testDraw/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  testDraw
+//
+//  Created by Admin on 2018/12/24.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
