26e8db5fd772d2ab4d242dd946e2396fa9fe5bb7
bpdnd <3438291751@qq.comgit config --global user.email 3438291751@qq.com>
2018-12-28 14:32:40 +0800

    添加 CAShapeLayer & UIBezierPath画线、画图

---
+++ testDraw/five/FiveViewController.m
@@ -0,0 +1,38 @@
+//
+//  FiveViewController.m
+//  testDraw
+//
+//  Created by Admin on 2018/12/28.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+#import "FiveViewController.h"
+
+@interface FiveViewController ()
+
+@end
+
+@implementation FiveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    //参考：https://www.cnblogs.com/jaesun/p/iOS-CAShapeLayerUIBezierPath-hua-xian.html
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
